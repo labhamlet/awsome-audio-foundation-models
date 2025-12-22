b396b376dbe649f6d9e4683bd9b4accbe6c9a2ac
root-goksenin <Goksenin.yuksel@outlook.com>
2025-12-22 11:35:47 +0100

    Add WavLM and run

---
+++ HuggingfaceModels/WavLM/hear_configs/wavlm.py
@@ -0,0 +1,14 @@
+from hear_api.wavLM import RuntimeWavLM
+import torch
+
+def load_model(*args, **kwargs):
+    model_size = kwargs.get("model_size", "base-plus")
+    model = RuntimeWavLM(model_size=model_size)
+    return model
+
+def get_scene_embeddings(audio, model):
+    return model.get_scene_embeddings(audio)
+
+
+def get_timestamp_embeddings(audio, model):
+    return model.get_timestamp_embeddings(audio)
