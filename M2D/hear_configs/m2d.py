f69a952b6f2e88e0c7cfd4880fcd79e7d566b7ff
root-goksenin <Goksenin.yuksel@outlook.com>
2025-12-22 20:44:16 +0100

    Add M2D

---
+++ M2D/hear_configs/m2d.py
@@ -0,0 +1,12 @@
+from hear_api.runtime import RuntimeM2D
+
+def load_model(*args, **kwargs):
+    model_size = kwargs.get("model_size", "base")
+    model = RuntimeM2D(model_size=model_size)
+    return model
+
+def get_scene_embeddings(audio, model):
+    return model.get_scene_embeddings(audio)
+
+def get_timestamp_embeddings(audio, model):
+    return model.get_timestamp_embeddings(audio)
