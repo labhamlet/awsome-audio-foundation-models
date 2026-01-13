75a12d0c786a5d531abec3a17baecc3b7fc87518
Göksenin Yüksel <121460943+root-goksenin@users.noreply.github.com>
2026-01-13 12:27:42 +0100

    Update gram_ambisonics.py

--- HuggingfaceModels/GRAM-Ambisonics/hear_configs/gram_ambisonics.py
+++ HuggingfaceModels/GRAM-Ambisonics/hear_configs/gram_ambisonics.py
@@ -2,7 +2,13 @@ from hear_api.runtime import RuntimeGRAMAmbisonics
 
 def load_model(*args, **kwargs):
     model_size = kwargs.get("model_size", "base")
-    model = RuntimeGRAMAmbisonics(model_size=model_size)
+    is_sn3d = kwargs.get("sn3d", False)
+    is_coord_normal = kwargs.get("ncord", False)
+
+    model = RuntimeGRAMAmbisonics(model_size=model_size,
+                                  is_sn3d = is_sn3d,
+                                  is_coord_normal = is_coord_normal
+                                  )
     return model
 
 def get_scene_embeddings(audio, model):
