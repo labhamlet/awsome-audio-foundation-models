44b92feca9a17d7ca9fa8c77b3e752814324d0ef
Göksenin Yüksel <121460943+root-goksenin@users.noreply.github.com>
2026-01-13 12:36:35 +0100

    Update gram_ambisonics.py

--- HuggingfaceModels/GRAM-Ambisonics/hear_configs/gram_ambisonics.py
+++ HuggingfaceModels/GRAM-Ambisonics/hear_configs/gram_ambisonics.py
@@ -2,9 +2,11 @@ from hear_api.runtime import RuntimeGRAMAmbisonics
 
 def load_model(*args, **kwargs):
     model_size = kwargs.get("model_size", "base")
-    is_sn3d = kwargs.get("sn3d", False)
-    is_coord_normal = kwargs.get("ncord", False)
+    is_sn3d = "true" == kwargs.get("sn3d", "false").lower()
+    is_coord_normal = "true" == kwargs.get("ncords", "false").lower()
 
+    print("SN3D: ", is_sn3d)
+    print("Normal Coord: ", is_coord_normal)
     model = RuntimeGRAMAmbisonics(model_size=model_size,
                                   is_sn3d = is_sn3d,
                                   is_coord_normal = is_coord_normal
